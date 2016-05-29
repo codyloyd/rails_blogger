8fc8cf407f302aa70e9748b6e42b0cd35e8e2abb
codyloyd <codyloyd@gmail.com>
2016-05-28 21:12:43 -0500

    deploy?

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430210702) do
+ActiveRecord::Schema.define(version: 20160501013548) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
